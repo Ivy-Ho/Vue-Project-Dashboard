1954e5988167d31c02a3911923140a0a0a9ed332
Ivy-Ho <ivycodefive@gmail.com>
2020-09-12 21:01:13 +0800

    update 2020-09-12 21:01:13

---
+++ js/chunk-2d22d746.59038c2c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h2",{staticClass:"text-center"},[t._v("這裡是前台-關於我們頁面")])])}],a=n("2877"),l={},r=Object(a["a"])(l,s,c,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.59038c2c.js.map
